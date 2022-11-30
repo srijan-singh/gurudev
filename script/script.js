9ce6bf09b748d250e7cc0281d0727e11a51b44f7
unknown <srijanssj5goku@gmail.com>
2022-11-30 16:42:28 +0530

    Version: 1

---
+++ script/script.js
@@ -0,0 +1,78 @@
+var content = "";
+
+function getKeyword(input)
+{
+	var result = "";
+
+	for(i=0; i<input.length; i++)
+	{
+		if(input==' ')
+		{
+			result+="%20";
+		}
+
+		else
+		{
+			result+=input[i];
+		}
+	}
+
+	return result;
+}
+
+function getResponse(input)
+{
+	var result = "";
+
+	for(i=0; i<input.length; i++)
+	{
+		if(input[i] == '\n')
+		{
+			result+="<br />"
+		}
+		else
+		{
+			result+=input[i];
+		}
+	}
+
+	return result;
+}
+
+async function SubmitVars() {
+
+	const keyword = getKeyword(document.getElementById("user_input").value);
+
+	const url = 'https://l0a45z.deta.dev/result/?user_input='+keyword
+	const response = await fetch(url,{
+		method:"GET",
+		headers: {"Content-type": "application/json; charset=UTF-8"}
+	})
+
+	const responseText = await response.text();
+	console.log(responseText); // logs 'OK'
+	var index_page = document.getElementById("answer"); 
+	index_page.style.backgroundColor = "white"; 
+	if(JSON.stringify(responseText).indexOf('overlap') > -1){index_page.style.color = "red"};
+	result = JSON.parse(responseText)
+	console.log(result["data"])
+
+	content = result["data"]
+
+	p_res = getResponse(result["data"])
+
+	index_page.innerHTML = (p_res);
+}
+
+
+async function Copy() {
+	try {
+	  await navigator.clipboard.writeText(content);
+	  console.log(content)
+	  /* Resolved - text copied to clipboard successfully */
+	} catch (err) {
+	  console.error('Failed to copy: ', err);
+	  /* Rejected - text failed to copy to the clipboard */
+	}
+  }
+
